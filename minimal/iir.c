540a15b104e41f3eaeda54c77d438793fd3e81fb
AmalizzyDHY <amalizzy.dhy@gmail.com>
2025-03-05 08:55:42 +0100

    message

--- minimal/iir.c
+++ minimal/iir.c
@@ -1,8 +1,8 @@
 #include "iir.h"
 
-
 absorp iirTest(char* filename){
-	absorp	res;
+    printf("Poulpe1\n");
+	absorp	res = {.acr = 0.0, .acir = 0, .dcr = 0, .dcir = 0};
     FILE* myfile = initFichier(filename);
 
     /*Verification de louverture du fichier
@@ -13,37 +13,46 @@ absorp iirTest(char* filename){
         res.dcr = 0;
         res.dcir = 0;
     }*/
-
+    printf("Poulpe2\n");
     /*Creation des absorp pour les calculs*/
-    absorp last_x;
-    absorp last_y;
-    absorp current_x;
+    absorp last_x = {.acr = 0.0, .acir = 0, .dcr = 0, .dcir = 0};
+    absorp last_y = {.acr = 0.0, .acir = 0, .dcr = 0, .dcir = 0};
+    absorp current_x = {.acr = 0.0, .acir = 0, .dcr = 0, .dcir = 0};
 
     /*Initialisation des variables*/
-    last_y.acr = 0;
-    last_y.acir = 0;
-    last_y.dcr = 0;
-    last_y.dcir = 0;
+    printf("last_y    => acr : %f, dcr : %f, acir : %f, dcir : %f\n", last_y.acr, last_y.dcr, last_y.acir, last_y.dcir);
+    printf("last_x    => acr : %f, dcr : %f, acir : %f, dcir : %f\n",last_x.acr, last_x.dcr, last_x.acir, last_x.dcir);
+    printf("current_x => acr : %f, dcr : %f, acir : %f, dcir : %f\n",current_x.acr, current_x.dcr, current_x.acir, current_x.dcir);
+    printf("Poulpe3\n\n");
 
-    last_x.acr = 0;
-    last_x.acir = 0;
-    last_x.dcr = 0;
-    last_x.dcir = 0;
+    bool condition = true;
 
     while(fgetc(myfile) != EOF){
-        /*Lecture ligne par ligne*/
+
+        if(condition){
+            condition = false;
+            fseek(myfile, SEEK_SET, 0);
+        }
+
+        //Lecture ligne par ligne
         current_x = lireFichier(myfile);
+        printf("current_x => acr : %f, dcr : %f, acir : %f, dcir : %f\n",current_x.acr, current_x.dcr, current_x.acir, current_x.dcir);
+        //printf("last_x    => acr : %f, dcr : %f, acir : %f, dcir : %f\n",last_x.acr, last_x.dcr, last_x.acir, last_x.dcir);
+        //printf("last_y    => acr : %f, dcr : %f, acir : %f, dcir : %f\n\n",last_y.acr, last_y.dcr, last_y.acir, last_y.dcir);
 
-        /*On passe dans le filtre*/
+        //On passe dans le filtre
         res = iir(current_x, last_x, last_y);
+        //printf("current_y => acr : %f, dcr : %f, acir : %f, dcir : %f\n\n",res.acr, res.dcr, res.acir, res.dcir);
 
-        /*On fait un decalage*/
+        //On fait un decalage
         last_x = current_x;
         last_y = res;
     }
 
     /*Fermeture du fichier*/
+    printf("Presque presque fin\n");
     finFichier(myfile);
+    printf("Presque fin\n\n");
     return res;
 
 }
@@ -59,3 +68,7 @@ absorp iir(absorp current_x, absorp last_x, absorp last_y){
 
     return current_y;
 }
+/*
+absorp lireMyFile(FILE* pf, int line){
+
+}*/
--- minimal/main_sim.c
+++ minimal/main_sim.c
@@ -1,8 +1,13 @@
 #include "fir.h"
+#include "iir.h"
 
 int main(){
 
-    firTest("log/log1.dat");
+    printf("Bonjour Mr Octopus\n");
+    absorp bobby = iirTest("log/log1_fir.dat");
+    printf("last y    => acr : %d, dcr : %d, acir : %d, dcir : %d\n",bobby.acr, bobby.dcr, bobby.acir, bobby.dcir);
+    printf("resultat  => acr : 732, dcr : 2048, acir : 1525, dcir : 2000\n",bobby.acr, bobby.dcr, bobby.acir, bobby.dcir);
+    printf("\nAu revoir Mr Octopus\n");
 
     return 0;
 }
